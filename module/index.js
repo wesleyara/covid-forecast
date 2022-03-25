403392b002dd22c6fe7b92ac39071d88ff4e1bba
wesleyara <wes1gom@gmail.com>
2022-03-25 17:03:36 -0300

    feat: Adicionando variavel ao calculo

--- module/index.js
+++ module/index.js
@@ -1,25 +1,57 @@
 const POPULATION = 50000;
 const RATE = -0.00000493723;
 
-module.exports = function forestCovid(t) {
-  if (t <= 1) {
+module.exports = function forestCovid(d) {
+  if (d <= 1) {
     return 0;
   }
 
   const dataDays = [];
+  const peopleCured = [];
 
-  for (let i = 0; i < t; i++) {
-    const result =
-      Math.floor(
+  for (let i = 0; i < d; i++) {
+    if (i < 13) {
+      const result =
+        Math.floor(
+          100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * (i + 1))),
+        ) -
+        Math.floor(
+          100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * i)),
+        );
+
+      const total = Math.floor(
         100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * (i + 1))),
-      ) -
-      Math.floor(100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * i)));
+      );
 
-    const total = Math.floor(
-      100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * (i + 1))),
-    );
+      dataDays.push(`${i + 1} -> ${result}, total de casos: ${total}`);
 
-    dataDays.push(`${i + 1} -> ${result}, total de casos: ${total}`);
+      peopleCured.push(result);
+    } else {
+      const total = Math.floor(
+        (100000 - peopleCured[i - 13] * 2) /
+          (2 +
+            (50000 - peopleCured[i - 13]) *
+              Math.exp((POPULATION - peopleCured[i - 13]) * RATE * (i + 1))),
+      );
+
+      const result =
+        Math.floor(
+          (100000 - peopleCured[i - 13] * 2) /
+            (2 +
+              (50000 - peopleCured[i - 13]) *
+                Math.exp((POPULATION - peopleCured[i - 13]) * RATE * (i + 1))),
+        ) -
+        Math.floor(
+          (100000 - peopleCured[i - 13] * 2) /
+            (2 +
+              (50000 - peopleCured[i - 13]) *
+                Math.exp((POPULATION - peopleCured[i - 13]) * RATE * i)),
+        );
+
+      peopleCured.push(result);
+
+      dataDays.push(`${i + 1} -> ${result}, total de casos: ${total}`);
+    }
   }
 
   return dataDays;
