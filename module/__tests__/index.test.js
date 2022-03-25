aef2a5f72c1185bb7eac9949860e4320f07118c2
wesleyara <wes1gom@gmail.com>
2022-03-25 15:39:48 -0300

    feat: Adicionando retorno no modelo desejado

--- module/__tests__/index.test.js
+++ module/__tests__/index.test.js
@@ -1,15 +1,26 @@
-const previewCovid = require("../index");
+const forestCovid = require("../index");
 
 describe("Preview Covid", () => {
   test("Verificar 10 dias", () => {
-    expect(previewCovid(10)).toBe(23);
+    expect(forestCovid(10)).toStrictEqual([
+      "1 -> 0, total de casos: 2",
+      "2 -> 1, total de casos: 3",
+      "3 -> 1, total de casos: 4",
+      "4 -> 1, total de casos: 5",
+      "5 -> 1, total de casos: 6",
+      "6 -> 2, total de casos: 8",
+      "7 -> 3, total de casos: 11",
+      "8 -> 3, total de casos: 14",
+      "9 -> 4, total de casos: 18",
+      "10 -> 5, total de casos: 23",
+    ]);
   });
 
   test("Verificar 0 dias", () => {
-    expect(previewCovid(0)).toBe(0);
+    expect(forestCovid(0)).toBe(0);
   });
 
   test("Verificar dias inválidos", () => {
-    expect(previewCovid(-10)).toBe(0);
+    expect(forestCovid(-10)).toBe(0);
   });
 });
--- module/index.js
+++ module/index.js
@@ -1,11 +1,26 @@
-const RATE = -0.246871374;
+const POPULATION = 50000;
+const RATE = -0.00000493723;
 
-module.exports = function previewCovid(t) {
+module.exports = function forestCovid(t) {
   if (t <= 1) {
     return 0;
   }
 
-  const preview = Math.floor(100004 / (2 + 50000 * Math.exp(RATE * t)));
+  const dataDays = [];
 
-  return preview;
+  for (let i = 0; i < t; i++) {
+    const result =
+      Math.floor(
+        100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * (i + 1))),
+      ) -
+      Math.floor(100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * i)));
+
+    const total = Math.floor(
+      100004 / (2 + 50000 * Math.exp((POPULATION + 2) * RATE * (i + 1))),
+    );
+
+    dataDays.push(`${i + 1} -> ${result}, total de casos: ${total}`);
+  }
+
+  return dataDays;
 };
